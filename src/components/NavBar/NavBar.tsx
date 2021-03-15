dd81a2276a8ca09a5b24e87ea1fc1bd675e44b5b
zenocoz <federico.soncini@gmail.com>
2021-03-15 10:39:26 +0100

    styled modal

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,12 @@
-import React from "react"
-import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap"
+import React, { useState } from "react"
+import { Navbar, Nav, Form, Button, FormControl, Modal } from "react-bootstrap"
 import letterboxd from "../../assets/letterboxd-logo-1000px.png"
 import "./NavBar.css"
 
 const NavBar = () => {
+  const handleClose = () => {}
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <div>
       {" "}
@@ -11,7 +14,10 @@ const NavBar = () => {
         <img src={letterboxd} />
         <Nav className="mr-auto nav-items">
           <Nav.Link href="#home">SIGN IN</Nav.Link>
-          <Nav.Link href="#features">CREATE ACCOUNT</Nav.Link>
+          <Nav.Link onClick={() => setShowModal(true)}>
+            {" "}
+            CREATE ACCOUNT
+          </Nav.Link>
           <Nav.Link href="#pricing">FILMS</Nav.Link>
           <Nav.Link href="#pricing">LISTS</Nav.Link>
           <Nav.Link href="#pricing">MEMBERS</Nav.Link>
@@ -20,6 +26,51 @@ const NavBar = () => {
           <FormControl type="text" className="mr-sm-2 search-bar" />
         </Form>
       </Navbar>
+      <Modal
+        dialogClassName="sign-in"
+        show={showModal}
+        onHide={() => {
+          setShowModal(false)
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>JOIN LETTERBOXD</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Username" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="I am at least 16 years old and I accept the terms of use"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="I accept the Privacy Policy and I accept to the processing of my personal
+              information in accordance with it."
+              />
+            </Form.Group>
+            <Button variant="success" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -9,7 +9,7 @@ const SignUp = () => {
         <Col>
           <Jumbotron></Jumbotron>
           <div className="welcome-texts">
-            <h1>Track films you watched.</h1>
+            <h1>Track films you've watched.</h1>
             <h1>Save those you want to see.</h1>
             <h1>Tell your friends what's good.</h1>
             <Button variant="success">GET STARTED - IT'S FREE!</Button>
