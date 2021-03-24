2beed69a031e42e2cf2fe30989b7d742b10ed0ac
zenocoz <federico.soncini@gmail.com>
2021-03-24 19:10:13 +0100

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import { Route } from "react-router-dom"
 import NavBar from "./components/NavBar/NavBar"
-
 import { Container } from "react-bootstrap"
 import Landing from "./pages/Landing/Landing"
 import Home from "./pages/Home/Home"
