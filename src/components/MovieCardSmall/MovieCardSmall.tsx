5fdecbe85349b94ff6851a1239c4ea0db55bdea2
zenocoz <federico.soncini@gmail.com>
2021-04-08 22:20:56 +0200

    fixed watching system

--- src/components/MovieCardSmall/MovieCardSmall.tsx
+++ src/components/MovieCardSmall/MovieCardSmall.tsx
@@ -2,15 +2,14 @@ import React from "react"
 import "./MovieCardSmall.css"
 import { IMovieCardSmallProps } from "./interface"
 import { Col } from "react-bootstrap"
+import { useHistory } from "react-router"
 
-const MovieCardSmall = ({ Poster }: IMovieCardSmallProps) => {
+const MovieCardSmall = ({ Poster, imdbID }: IMovieCardSmallProps) => {
+  const history = useHistory()
   return (
     <div className="col md-8 mb-4 small-card">
-      <img src={Poster} />
+      <img src={Poster} onClick={() => history.push(`/film/${imdbID}`)} />
     </div>
-    // <Col>
-    //   <img src={Poster} />
-    // </Col>
   )
 }
 
--- src/components/MovieCardSmall/interface.ts
+++ src/components/MovieCardSmall/interface.ts
@@ -1,3 +1,4 @@
 export interface IMovieCardSmallProps {
   Poster: string
+  imdbID: string
 }
